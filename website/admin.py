8ca30a77a9b4b384cf25c8d2f44489fef9e9f3e8
miladzeinali <58866441+miladzeinali@users.noreply.github.com>
2020-08-08 00:38:18 +0430

    tyre handler model refine

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import TyreHandler
 # Register your models here.
+
+
+class TyreHandlerAdmin(admin.ModelAdmin):
+    list_display = ('id','code','model_Year')
+    search_fields = ('id','code','model_Year','name')
+
+
+admin.site.register(TyreHandler,TyreHandlerAdmin)
--- website/models.py
+++ website/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 
 class TyreHandler(models.Model):
-    name=models.TextField(max_length=15,null=True,blank=True)
-    code=models.TextField(max_length=15,null=True,blank=True)
-    modelYear=models.TextField(max_length=15,null=True,blank=True)
-    capacityKg=models.PositiveSmallIntegerField(null=True,blank=True,default=0)
-    capacityLbs=models.PositiveSmallIntegerField(null=True,blank=True,default=0)
-    OpeningRangeMmMin=models.PositiveSmallIntegerField(null=True,blank=True,default=0)
-    OpeningRangeMmMax=models.PositiveSmallIntegerField(null=True,blank=True,default=0)
-    OpeningRangeInMin=models.PositiveSmallIntegerField(null=True,blank=True,default=0)
-    OpeningRangeInMax=models.PositiveSmallIntegerField(null=True,blank=True,default=0)
-    sideShiftMm=models.PositiveSmallIntegerField(null=True,blank=True,default=0)
-    sideShiftIn=models.PositiveSmallIntegerField(null=True,blank=True,default=0)
+    name=models.CharField(max_length=15,null=True,blank=True)
+    code=models.CharField(max_length=15,null=True,blank=True)
+    model_Year=models.CharField(max_length=6,null=True,blank=True)
+    capacity_Kg=models.PositiveSmallIntegerField(null=True,blank=True,default=0)
+    capacity_Lbs=models.PositiveSmallIntegerField(null=True,blank=True,default=0)
+    OpeningRange_Mm_Min=models.PositiveSmallIntegerField(null=True,blank=True,default=0)
+    OpeningRange_Mm_Max=models.PositiveSmallIntegerField(null=True,blank=True,default=0)
+    OpeningRange_In_Min=models.PositiveSmallIntegerField(null=True,blank=True,default=0)
+    OpeningRange_In_Max=models.PositiveSmallIntegerField(null=True,blank=True,default=0)
+    sideShift_Mm=models.PositiveSmallIntegerField(null=True,blank=True,default=0)
+    sideShift_In=models.PositiveSmallIntegerField(null=True,blank=True,default=0)
     bodyRotation=models.PositiveSmallIntegerField(null=True,blank=True,default=0)
     PadsRotation=models.PositiveSmallIntegerField(null=True,blank=True,default=0)
     DesignTypeChoice=(
@@ -19,8 +19,8 @@ class TyreHandler(models.Model):
         ('telescopic','telescopic'),
         ('parallelogram','parallelogram'),
     )
-    DesignType=models.TextField(max_length=15,default='flat')
+    DesignType=models.CharField(max_length=15,choices=DesignTypeChoice,default='flat')
     image=models.ImageField(null=True,blank=True,upload_to='tyrehandler/')
-    description=models.TextField(max_length=50,null=True,blank=True)
+    description=models.CharField(max_length=50,null=True,blank=True)
     priceToman=models.PositiveSmallIntegerField(null=True,blank=True,default=0)
     priceDollar=models.PositiveSmallIntegerField(null=True,blank=True,default=0)
