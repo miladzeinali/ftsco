295bef0445a30e3fc3f9ae16e2c5121525907a73
miladzeinali <58866441+miladzeinali@users.noreply.github.com>
2020-08-08 11:57:25 +0430

    strut handler model added

--- website/admin.py
+++ website/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import TyreHandler,CylinderHandler
+from .models import TyreHandler,CylinderHandler,StrutHandler
 # Register your models here.
 
 
@@ -11,5 +11,10 @@ class CylinderHandlerAdmin(admin.ModelAdmin):
     list_display = ('id','code','model_Year')
     search_fields = ('id','code','model_Year','name')
 
+class StrutHandlerAdmin(admin.ModelAdmin):
+    list_display = ('id','code','model_Year')
+    search_fields = ('id','code','model_Year','name')
+
 admin.site.register(TyreHandler,TyreHandlerAdmin)
 admin.site.register(CylinderHandler,CylinderHandlerAdmin)
+admin.site.register(StrutHandler,StrutHandlerAdmin)
