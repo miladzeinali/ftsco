6678bbe2b43af4c9ace2e9c057fee81161c6fd18
miladzeinali <58866441+miladzeinali@users.noreply.github.com>
2020-08-08 13:17:18 +0430

    home views started

--- website/urls.py
+++ website/urls.py
@@ -5,4 +5,7 @@ from . import views
 app_name='website'
 urlpatterns = [
     path('',views.home,name='home'),
+    path('tyrehandlers',views.tyreHandler,name='tyreh'),
+    path('cylinderhandlers',views.cylinderHandler,name='cylinderh'),
+    path('struthandlers',views.strutHandler,name='struth'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render
+from .models import TyreHandler,CylinderHandler,StrutHandler
 
 # Create your views here.
 
 
 def home(request):
-    return render(request,'pages/home.html',{'alo':[1,2]})
+    return render(request,'pages/home.html',{'alo':[]})
+
+def tyreHandler(request):
+    tyrehandlers=TyreHandler.objects.all()
+    return render(request,'webproduct/tyrehandlers.html',{'tyrehandler':tyrehandlers})
+
+def cylinderHandler(request):
+    cylinderhandlers=CylinderHandler.objects.all()
+    return render(request,'webproduct/cylinderhandlers.html',{'cylinderhandlers':cylinderhandlers})
+
+def strutHandler(request):
+    struthandlers=StrutHandler.objects.all()
+    return render(request,'webproduct/struthandlers.html',{'struthandlers':struthandlers})
