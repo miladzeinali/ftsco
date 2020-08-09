a1915bd51b39c978a32e28f2b9dbcfdd57caa937
sadegh <mhmdsadqd@gmail.com>
2020-08-09 20:40:57 +0430

    add new file to static and templates and fix home.html--sadegh

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 app_name = 'website'
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('',views.home,name='home'),
+    path('products/tyrehandlers',views.tyreHandler,name='tyreh'),
+    path('products/cylinderhandlers',views.cylinderHandler,name='cylinderh'),
+    path('products/struthandlers',views.strutHandler,name='struth'),
 ]
