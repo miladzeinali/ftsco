70331d73a800dc49f2d3408e0910ec71f30259f3
sadegh <mhmdsadqd@gmail.com>
2020-08-12 14:33:50 +0430

    fix productList template --sadegh

--- FtskCo/urls.py
+++ FtskCo/urls.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
 """FtskCo URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -17,6 +20,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('website.urls')),
-]
+                  path('admin/', admin.site.urls),
+                  path('', include('website.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
