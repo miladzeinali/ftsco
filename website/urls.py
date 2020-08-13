ad7dff6b284515d6749f95e6a5228e87071914b4
sadegh <mhmdsadqd@gmail.com>
2020-08-13 20:16:46 +0430

    pull as milad va flush and migrate --sadegh

--- website/urls.py
+++ website/urls.py
@@ -3,8 +3,12 @@ from . import views
 
 app_name = 'website'
 urlpatterns = [
-    path('',views.home,name='home'),
-    path('products/tyrehandlers',views.tyreHandler,name='tyreh'),
-    path('products/cylinderhandlers',views.cylinderHandler,name='cylinderh'),
-    path('products/struthandlers',views.strutHandler,name='struth'),
+    path('', views.home, name='home'),
+    path('products/tyrehandlers', views.tyreHandler, name='tyreh'),
+    path('products/cylinderhandlers', views.cylinderHandler, name='cylinderh'),
+    path('products/struthandlers', views.strutHandler, name='struth'),
+    path('products/detailcontrol/<int:id>/<str:type>/', views.productcontrol, name='productcontrol'),
+    path('products/tyrehandlers/<int:id>/', views.tyrehandler_detail, name='tyrehdetail'),
+    path('products/struthandlers/<int:id>/', views.struthandler_detail, name='strutdetail'),
+    path('products/cylinderhandlers/<int:id>/', views.cylinderhandler_detail, name='cylinderdetail'),
 ]
