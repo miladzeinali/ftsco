e21bb3c08bca2c63facf19f5194d6540934e182a
sadegh <mhmdsadqd@gmail.com>
2020-08-13 08:12:47 +0430

    not change --sadegh

--- website/views.py
+++ website/views.py
@@ -1,23 +1,27 @@
 from django.shortcuts import render
-from .models import TyreHandler,CylinderHandler,StrutHandler
+from .models import TyreHandler, CylinderHandler, StrutHandler
+
 
 # Create your views here.
 
 
 def home(request):
-    return render(request,'pages/home.html',{'alo':[]})
+    return render(request, 'pages/home.html', {'alo': []})
+
 
 def tyreHandler(request):
-    tyrehandlers=TyreHandler.objects.all()
-    count=len(tyrehandlers)
-    return render(request, 'webproduct/listproduct.html', {'products':tyrehandlers, 'count':count})
+    tyrehandlers = TyreHandler.objects.all()
+    count = len(tyrehandlers)
+    return render(request, 'webproduct/listproduct.html', {'products': tyrehandlers, 'count': count})
+
 
 def cylinderHandler(request):
-    cylinderhandlers=CylinderHandler.objects.all()
-    count=len(cylinderhandlers)
-    return render(request,'webproduct/listproduct.html',{'products':cylinderhandlers,'count':count})
+    cylinderhandlers = CylinderHandler.objects.all()
+    count = len(cylinderhandlers)
+    return render(request, 'webproduct/listproduct.html', {'products': cylinderhandlers, 'count': count})
+
 
 def strutHandler(request):
-    struthandlers=StrutHandler.objects.all()
-    count=len(struthandlers)
-    return render(request,'webproduct/listproduct.html',{'products':struthandlers,'count':count})
+    struthandlers = StrutHandler.objects.all()
+    count = len(struthandlers)
+    return render(request, 'webproduct/listproduct.html', {'products': struthandlers, 'count': count})
