c77e58856cdf5827d91a73d207d1086d9ab92cab
miladzeinali <58866441+miladzeinali@users.noreply.github.com>
2020-11-28 18:47:52 +0330

    push for sadegh start

--- FtskCo/settings.py
+++ FtskCo/settings.py
@@ -35,11 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'enweb.apps.EnwebConfig',
     'website.apps.WebsiteConfig',
     'ckeditor',
-    'enweb',
-    'website',
-    'enweb.apps.EnwebConfig',
 ]
 
 MIDDLEWARE = [
@@ -80,7 +78,7 @@ WSGI_APPLICATION = 'FtskCo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / "db.sqlite3"),
     }
 }
 # DATABASES = {
