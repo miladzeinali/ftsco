6f7aa0d6a879498d8a3f579f450651ff20ac6978
sadegh <mhmdsadqd@gmail.com>
2021-08-23 23:43:55 +0430

    add template mags --sadegh

--- mag/views.py
+++ mag/views.py
@@ -1,35 +1,37 @@
 from django.shortcuts import render
 from .models import *
+import random
 
 def home(request):
-    post = Post.objects.all()
-    posts = random.choice(post)
-    news = post[0:5]
-    top = sorted(post,key=lambda x: x.likes)
+    posts = Post.objects.all()
+    # posts = random.choice(post)
+    news = posts[0:5]
+    top = sorted(posts,key=lambda x: x.likes)
     slider = Slider.objects.all()[0:3]
-    newpro = NewsProduct.objects.latest()
-    return render(request,'home.html',{'posts':posts,'news':news,'top':top,'slider':slider,'newspro':newpro})
+    # newpro = NewsProduct.objects.latest()
+    newpro = NewsProduct.objects.all()
+    return render(request,'mags/home.html',{'posts':posts,'news':news,'top':top,'slider':slider,'newspro':newpro})
 
 def Detail(request,id):
     post = Post.objects.get(id=id)
     comments = Comment.objects.filter(post=post,approved=True)
-    return render(request,'detail.html',{'post':post,'comments':comments})
+    return render(request,'mags/detail.html',{'post':post,'comments':comments})
 
 def technology(request):
-    posts = Post.objects.filter(category='technology')
-    return render(request,'result.html',{'posts':posts})
+    posts = Post.objects.filter(category='1')
+    return render(request,'mags/technology.html',{'posts':posts})
 
 def safety(request):
-    posts = Post.objects.filter(category='safety')
-    return render(request,'result.html',{'posts':posts})
+    posts = Post.objects.filter(category='2')
+    return render(request,'mags/safety.html',{'posts':posts})
 
 def news(request):
-    posts = Post.objects.filter(category='news')
-    return render(request,'result.html',{'posts':posts})
+    posts = Post.objects.filter(category='3')
+    return render(request,'mags/news.html',{'posts':posts})
 
 def exhibition(request):
-    posts = Post.objects.filter(category='exhibition')
-    return render(request,'result.html',{'posts':posts})
+    posts = Post.objects.filter(category='4')
+    return render(request,'mags/exhibition.html',{'posts':posts})
 
 def comment(request,id):
     name = request.POST['name']
