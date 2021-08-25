1d25273ddd89e5141cf5fcc9645b92b617cd0353
shiateam <mhmdsadqd@gmail.com>
2021-08-25 22:13:30 +0430

    I dont change just commit for push --sadegh

--- mag/admin.py
+++ mag/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id','category')
+    search_fields = ('id','category')
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id','subtitle','category')
+    search_fields = ('id','title')
+
+class SliderAdmin(admin.ModelAdmin):
+    list_display = ('id','text')
+    search_fields = ('id','text')
+
+class NewsProductAdmin(admin.ModelAdmin):
+    list_display = ('id','name')
+    search_fields = ('id','name')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id','name','comment','approved')
+    search_fields = ('id','name','comment')
+
+class MailAdmin(admin.ModelAdmin):
+    list_display = ('id','name','title')
+    search_fields = ('id','name','title')
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Post,PostAdmin)
+admin.site.register(Slider,SliderAdmin)
+admin.site.register(NewsProduct,NewsProductAdmin)
+admin.site.register(Comment,CommentAdmin)
+admin.site.register(Mail,MailAdmin)
